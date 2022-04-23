1e6cb8239ce319b71d0fb7da32b6da6a1c8fc7e6
Hasanagha Azad <hasanaghaazad@MacBook-Air-Hasanagha.local>
2022-04-23 18:43:05 +0400

    fixed target types on links

--- resources/views/includes/certificates.blade.php
+++ resources/views/includes/certificates.blade.php
@@ -1,21 +1,21 @@
 <div class="row">
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/a7284004631e9af93e2ea1ba2de951f1839b988094cfecc84ef90f4bcf9db8fa" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/a7284004631e9af93e2ea1ba2de951f1839b988094cfecc84ef90f4bcf9db8fa" target="_blank">
                                 <img src="/img/certificates/WebProgrammingFoundations.jpeg" class="card-img-top" alt="Linkedin Web Programming Foundations Learning Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/fcaf97e351b5a34e6b1b7b5a03dba08c0398ec88ec743dc19db7ce49f0164a78" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/fcaf97e351b5a34e6b1b7b5a03dba08c0398ec88ec743dc19db7ce49f0164a78" target="_blank">
                                 <img src="/img/certificates/HTML-Essential.jpg" class="card-img-top" alt="Linkedin HTML-Essential Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/d66e199a1beeece918d2eb962323587e952c62c594d2089a3c9260d0d156b424" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/d66e199a1beeece918d2eb962323587e952c62c594d2089a3c9260d0d156b424" target="_blank">
                                 <img src="/img/certificates/CSS-Introduction.jpg" class="card-img-top" alt="Linkedin Introduction to CSS Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
@@ -24,21 +24,21 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/268d876dac9b9197a54277363864c910a5e013b0b2f95583e57f29220f265fa3" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/268d876dac9b9197a54277363864c910a5e013b0b2f95583e57f29220f265fa3" target="_blank">
                                 <img src="/img/certificates/CSS-Essential.jpg" class="card-img-top" alt="Linkedin CSS-Essential Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/89ea5162cee5209f15625118a6f5003da0f23dbc4e164e1a6829b09898ab6510" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/89ea5162cee5209f15625118a6f5003da0f23dbc4e164e1a6829b09898ab6510" target="_blank">
                                 <img src="/img/certificates/JavaScriptEssential.jpeg" class="card-img-top" alt="Linkedin JavaScript Essential Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/c1970a647c5eab90bb0c56d6cb8396854ac2294c0fb30150c85e90ffd8fb8c26" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/c1970a647c5eab90bb0c56d6cb8396854ac2294c0fb30150c85e90ffd8fb8c26" target="_blank">
                                 <img src="/img/certificates/RESTAPILearning.jpeg" class="card-img-top" alt="Linkedin REST API Learning Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
@@ -47,21 +47,21 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/0997f11476fd27cbfb4ed5c49667f187318c929370e47a3ff71ccdd7e207a098" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/0997f11476fd27cbfb4ed5c49667f187318c929370e47a3ff71ccdd7e207a098" target="_blank">
                                 <img src="/img/certificates/NodejsLearning.jpeg" class="card-img-top" alt="Linkedin Node.js Learning Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/d4e94a2e6e3b362a5165511724e9ad6188cabe9d30ae71852e53b5db1c355bac" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/d4e94a2e6e3b362a5165511724e9ad6188cabe9d30ae71852e53b5db1c355bac" target="_blank">
                                 <img src="/img/certificates/NodejsDatabases.jpeg" class="card-img-top" alt="Linkedin Node.js Databases Learning Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/c1970a647c5eab90bb0c56d6cb8396854ac2294c0fb30150c85e90ffd8fb8c26" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/c1970a647c5eab90bb0c56d6cb8396854ac2294c0fb30150c85e90ffd8fb8c26" target="_blank">
                                 <img src="/img/certificates/NodejsExpressREST.jpeg" class="card-img-top" alt="Linkedin Node.js Express REST API Learning Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
@@ -70,14 +70,14 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/f9a97390460780c26b11b410691981ad5ad23f9bf356dc8f289440f3f5d59548" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/f9a97390460780c26b11b410691981ad5ad23f9bf356dc8f289440f3f5d59548" target="_blank">
                                 <img src="/img/certificates/WebDevelopersSearchTechniques.jpeg" class="card-img-top" alt="Linkedin Web Development Search Techniques Learning Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="card border-0 p-0 mb-3 mt-4">
-                            <a href="https://www.linkedin.com/learning/certificates/d62356af617b432ae6545905937d421668b8f787bb34b204e9e7a8ae7412b503" target="blank">
+                            <a href="https://www.linkedin.com/learning/certificates/d62356af617b432ae6545905937d421668b8f787bb34b204e9e7a8ae7412b503" target="_blank">
                                 <img src="/img/certificates/WebDevelopmentFullAndFront.jpeg" class="card-img-top" alt="Linkedin Web Development FullStack and FrontEnd Learning Training Certificate of Hasanagha Azad">
                             </a>
                         </div>
